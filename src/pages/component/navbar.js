77a3d1b14ef9acae3a0e8237868592a1785801fd
NandinhaFerreira1 <fnnanda3@gmail.com>
2024-10-14 15:39:45 +0000

    aula 14-10

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,10 +1,11 @@
 import {Container, Nav, Navbar} from 'react-bootstrap';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import style from "@/styles/Navbar.module.css"
 
 export default function Menu() {
   return (
-    <Navbar expand="sm" className="bg-danger-subtle">
-      <Container>
+    <Container>
+    <Navbar expand="sm" className={`${style.navGradient} px-3 my-0 border border-2 border border-primary rounded`} >
+      
         <Navbar.Brand href="/">6B</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -13,8 +14,8 @@ export default function Menu() {
             <Nav.Link href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        </Navbar>
       </Container>
-    </Navbar>
   );
 }
 
